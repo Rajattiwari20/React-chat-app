d18cd2ffa057538621a332c8a688ee65869857fe
Rajat Tiwari <rajat@truelancer.com>
2021-10-11 12:50:02 +0530

    Added NewConversation component

--- src/components/AdminDetail.js
+++ src/components/AdminDetail.js
@@ -1,19 +1,27 @@
 import  React, { useState } from 'react';
 import { Typography , Grid, Avatar, Button} from '@mui/material'
+import {useHistory} from 'react-router-dom'
 
 function AdminDetail(props) {
 
   const {data} = props
+
+  let history = useHistory()
+  const handelClick = () =>{
+    history.push("/")
+}
 
     return (
       <>
         { data && 
           <Grid container>
                 <Grid item md = {2} xs = {12} sm = {12}>
+                  <div onClick= {handelClick}>
                 <Avatar alt="Pic" src= {data.photo} />
+                  </div>
                 </Grid>
                 <Grid item md = {6} xs = {12} sm = {12} style = {{marginTop : "-10px" , color : "white" , cursor : "pointer"}}>
-                <h4 > {data.userName} </h4>
+                <h4 onClick= {handelClick} > {data.userName} </h4>
                 </Grid>
                 <Grid item md = {4} xs = {12} sm = {12} style = {{marginTop : "-10px" , color : "white", cursor : "pointer"}}>
                 <h4> {data.contactNo} </h4>
--- src/components/NewConversation.js
+++ src/components/NewConversation.js
@@ -6,9 +6,8 @@ function NewConversation() {
     return (
       <>
         <Grid container>
-                <Grid item md = {12} xs = {12} sm = {12} style = {{ minHeight : "80vh"}}>
-                    <h1>Start new chat</h1>
-                </Grid>
+                <Grid item md = {12} xs = {12} sm = {12} style = {{ minHeight : "90vh" ,backgroundImage: `url(${"https://cdn.dribbble.com/users/1354640/screenshots/7594295/media/91ca96954883a99dbe8b1fe3b8458d86.jpg?compress=1&resize=1200x900"})`}}>
+                  </Grid>
         </Grid>
       </>
     );
