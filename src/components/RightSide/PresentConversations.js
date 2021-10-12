b464f1dfc207dcfc5e85e6c9bdc9e741b195bf59
Rajat Tiwari <rajat@truelancer.com>
2021-10-12 10:24:20 +0530

    change BrowserRouter to HashRouter

--- src/components/RightSide/PresentConversations.js
+++ src/components/RightSide/PresentConversations.js
@@ -12,23 +12,23 @@ function PresentConversations(props) {
             <Grid item md = {12} xs = {12} sm = {12} style = {{minHeight : "90vh", backgroundColor : "#EAEEF3"}} >
               {/* using switch and Route provided by react-router-dom' for doing client side routing  */}
             <Switch>
-                  <Route path="rajattiwari20.github.io/React-chat-app/user2" exact>
+                  <Route path="/user2" exact>
                     <CurrentConversation userId = {2} />
                   </Route>
 
-                  <Route path="rajattiwari20.github.io/React-chat-app/user3" exact>
+                  <Route path="/user3" exact>
                     <CurrentConversation userId = {3}  />
                   </Route>
                   
-                  <Route path="rajattiwari20.github.io/React-chat-app/user4" exact>
+                  <Route path="/user4" exact>
                     <CurrentConversation userId = {4}  />
                   </Route>
                   
-                  <Route path="rajattiwari20.github.io/React-chat-app/user5" exact>
+                  <Route path="/user5" exact>
                     <CurrentConversation userId = {5}  />
                   </Route>
                   
-                  <Route path="rajattiwari20.github.io/React-chat-app/user6" exact>
+                  <Route path="/user6" exact>
                     <CurrentConversation userId = {6}  />
                   </Route>
                   
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { BrowserRouter} from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
-  //BrowserRouter is used for doing client side routing 
-  <BrowserRouter>
+  //HashRouter is used for doing client side routing 
+  <HashRouter>
         <App />
-    </BrowserRouter>,
+    </HashRouter>,
   document.getElementById('root')
 );
